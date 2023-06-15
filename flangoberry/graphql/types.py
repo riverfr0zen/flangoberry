df1bad840d5844b1cb6c14e31d52db9adf43def3
Irfan Baig <riverfr0zen@elitemail.org>
2023-06-15 00:15:10 -0400

    added BaseEdgeFieldsMixin

--- flangoberry/graphql/types.py
+++ flangoberry/graphql/types.py
@@ -30,3 +30,35 @@ class BaseVertexFieldsMixin:
                 v = to_py_date(v)
             converted_data[k] = v
         return cls(**converted_data)
+
+
+@strawberry.type
+class BaseEdgeFieldsMixin:
+    id: str
+    key: str
+    rev: str
+    frm: str
+    to: str
+    created: datetime
+    modified: datetime
+
+    @classmethod
+    def from_dbdoc(cls, doc: dict) -> dict:
+        """
+        Prepares data from the database for population into the Strawberry object:
+        * Converts the _key, _id, and _rev, _to, _from field names from the database to GQL-acceptable ones.
+        * Converts any fields destined for datetime attrs to Python datetimes.
+        """
+        obj_type_hints = get_type_hints(cls)
+        datetime_attrs = [k for k, v in obj_type_hints.items() if v is datetime]
+        # logger.debug(datetime_attrs)
+        converted_data = {}
+        for k, v in doc.items():
+            if k in ["_key", "_id", "_rev", "_to"]:
+                k = k.replace("_", "")
+            if k == "_from":
+                k = "frm"
+            if k in datetime_attrs:
+                v = to_py_date(v)
+            converted_data[k] = v
+        return cls(**converted_data)
--- flangoberry/tests/test_graphql.py
+++ flangoberry/tests/test_graphql.py
@@ -48,3 +48,39 @@ def test_vertex_from_dbdoc():
     assert isinstance(obj.created, datetime)
     assert isinstance(obj.modified, datetime)
     assert isinstance(obj.some_custom_datetime, datetime)
+
+
+@strawberry.type
+class MyEdgeType(types.BaseEdgeFieldsMixin):
+    animal: str
+    some_custom_datetime: datetime
+
+
+def test_edge_from_dbdoc():
+    doc = {
+        "_key": "somekey",
+        "_rev": "somerev",
+        "_id": "someid",
+        "_from": "somefrom",
+        "_to": "someto",
+        "animal": "eel",
+        "created": datetime.now().isoformat(),
+        "modified": datetime.now().isoformat(),
+        "some_custom_datetime": datetime.now().isoformat(),
+    }
+
+    obj = MyEdgeType.from_dbdoc(doc)
+    assert not hasattr(obj, "_key")
+    assert obj.key == doc["_key"]
+    assert not hasattr(obj, "_rev")
+    assert obj.rev == doc["_rev"]
+    assert not hasattr(obj, "_id")
+    assert obj.id == doc["_id"]
+    assert not hasattr(obj, "_from")
+    assert obj.frm == doc["_from"]
+    assert not hasattr(obj, "_to")
+    assert obj.to == doc["_to"]
+    assert obj.animal == "eel"
+    assert isinstance(obj.created, datetime)
+    assert isinstance(obj.modified, datetime)
+    assert isinstance(obj.some_custom_datetime, datetime)
